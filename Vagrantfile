587a131264c29fbef8645540c76de53bf4c3707e
eduardotristao <edu.tristao@gmail.com>
2019-09-03 15:45:21 -0300

    teste

--- Vagrantfile
+++ Vagrantfile
@@ -23,4 +23,5 @@ Vagrant.configure("2") do |config|
   config.vm.provision "shell", inline: "sudo apt-get install -y xfce4 virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11"
 # Permite qualquer um iniciar a GUI
   config.vm.provision "shell", inline: "sudo sed -i 's/allowed_users=.*$/allowed_users=anybody/' /etc/X11/Xwrapper.config"
+  config.vm.provision "shell", inline: "sudo reboot"
 end
--- setup.sh
+++ setup.sh
@@ -3,12 +3,12 @@
 echo "---- Iniciando instalacao do ambiente de Desenvolvimento PHP ---"
 
 echo "--- Instalando Visual Code ---"
-sudo apt update
-sudo apt install -y apt-transport-https wget
+sudo apt-get update
+sudo apt-get install -y apt-transport-https wget
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt update
-sudo apt install code -y
+sudo apt-get update
+sudo apt-get install code -y
 
 echo "[OK] --- Ambiente de desenvolvimento concluido ---"
 
@@ -26,8 +26,8 @@ echo "[OK] --- Ambiente de desenvolvimento concluido ---"
     wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
     RELEASE=$(lsb_release -cs)
     echo "deb http://apt.postgresql.org/pub/repos/apt/ ${RELEASE}"-pgdg main | sudo tee  /etc/apt/sources.list.d/pgdg.list
-    sudo apt update
-    sudo apt -y install postgresql-11
+    sudo apt-get update
+    sudo apt-get -y install postgresql-11
     echo ''
     echo "PostgreSQL instalado com sucesso. [$CHECK_SYMBOL]"
   fi
@@ -41,9 +41,9 @@ echo "[OK] --- Ambiente de desenvolvimento concluido ---"
     echo "PHP não encontrado. [$ERROR_SYMBOL]"
     echo 'Instalando PHP...'
     echo ''
-    sudo apt install -y software-properties-common
+    sudo apt-get install -y software-properties-common
     add-apt-repository ppa:ondrej/php
-    sudo apt install -y php7.1 php7.1-common php7.1-opcache php7.1-mcrypt php7.1-cli php7.1-gd php7.1-curl php7.1 php7.1-fpm
+    sudo apt-get install -y php7.1 php7.1-common php7.1-opcache php7.1-mcrypt php7.1-cli php7.1-gd php7.1-curl php7.1 php7.1-fpm
     php -v
     echo ''
     echo "PostgreSQL instalado com sucesso. [$CHECK_SYMBOL]"
@@ -54,6 +54,6 @@ curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
 # Instalando Nginx
-    apt-get update
-    apt-get install nginx
+    sudo apt-get update
+    sudo apt-get install nginx
     systemctl restart nginx.service
